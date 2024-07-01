7142d6d18d881f565d73942cb71c59ea2a074509
JOSE IGNACIO VARGAS JARAMILLO <135558719+Getz7@users.noreply.github.com>
2024-07-01 15:19:49 -0600

    Generacion de menus de cada rol, favicon, importe de fonts y reestructuracion de Css

---
+++ src/app/pages/badges-users/badges-users.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-badges-users',
+  standalone: true,
+  imports: [],
+  templateUrl: './badges-users.component.html',
+  styleUrl: './badges-users.component.scss'
+})
+export class BadgesUsersComponent {
+
+}
