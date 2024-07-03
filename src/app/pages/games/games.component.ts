13785b90aa421efd02be7a464b770425e4b29b4d
Daniel Gomez <dgnavarro06@gmail.com>
2024-07-02 18:05:59 -0600

    Simon Dice dificultad Facil Creado
    Componente de Simon Dice dificultad Facil creado
    Falta agregar sonidos de fallo y gane y siguiente secuencia

--- src/app/pages/games/games.component.ts
+++ src/app/pages/games/games.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { SequenceGameComponent } from "../../components/sequence-game/sequence-game.component";
+import { IGame } from "../../interfaces";
 
 @Component({
-  selector: 'app-games',
+  selector: "app-games",
   standalone: true,
-  imports: [],
-  templateUrl: './games.component.html',
-  styleUrl: './games.component.scss'
+  imports: [SequenceGameComponent],
+  templateUrl: "./games.component.html",
+  styleUrl: "./games.component.scss",
 })
 export class GamesComponent {
-
+  public gamesList: IGame[] = [
+    {
+      gameId: 1,
+      name: "Simon Dice Dificultad Facil",
+      description:
+        "Juego de Simon dice. Seguir la secuencia de colores que se presenta.",
+      levelDifficult: "Facil",
+      typeExercise: "Ejercicio Cognitivo",
+    },
+    {
+      gameId: 2,
+      name: "Simon Dice Dificultad Media",
+      description:
+        "Juego de Simon dice. Preisone el boton verde lo mas rapido posible..",
+      levelDifficult: "Medio",
+      typeExercise: "Ejercicio Cognitivo",
+    },
+  ];
 }
