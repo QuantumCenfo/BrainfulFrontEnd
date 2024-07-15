5ceb93a3a76c97293aca5e056d4af177dd8ffe1b
JOSE IGNACIO VARGAS JARAMILLO <135558719+Getz7@users.noreply.github.com>
2024-07-15 17:50:37 -0600

    Push cambios de matias

--- src/app/pages/Brainful/faq/faq.component.ts
+++ src/app/pages/Brainful/faq/faq.component.ts
@@ -12,8 +12,11 @@ import { CommonModule } from "@angular/common";
 })
 export class FaqComponent {
   faqItems = [
-    { question: "Tengo que estar enfermo?", answer: "No", isOpen: false },
-    { question: "No abrir", answer: "Gay si ve esto", isOpen: false },
+    { question: "¿Qué es esta aplicación?", answer: "Nuestra aplicación es una herramienta diseñada para ofrecer apoyo y recursos a personas que están enfrentando problemas de salud mental.", isOpen: false },
+    { question: "¿Cómo funciona la aplicación?", answer: "La aplicación proporciona acceso a herramientas de autoayuda, seguimiento del estado de ánimo, ejercicios de mindfulness", isOpen: false },
+    { question: "¿Cómo se protege mi información personal?", answer: "Tomamos la privacidad y seguridad muy en serio. Utilizamos encriptación de extremo a extremo para proteger tu información y nunca compartimos tus datos sin tu consentimiento.", isOpen: false },
+    { question: "¿Mis datos se mantendrán confidenciales?", answer: "Sí, todos tus datos son confidenciales y solo tú puedes acceder a ellos.", isOpen: false },
+    { question: "¿Qué tipo de ejercicios y herramientas están disponibles en la aplicación?", answer: "La aplicación ofrece una variedad de herramientas, incluyendo ejercicios de mindfulness, técnicas de respiración, seguimiento del estado de ánimo, diarios, y recursos educativos.", isOpen: false },
   ];
 
   toggleAccordion(item: any) {
