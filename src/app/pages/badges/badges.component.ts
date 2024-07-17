76316fca9c9458dfbfc20467897ad4a2a9dcfbf6
Daniel Gomez <dgnavarro06@gmail.com>
2024-07-16 22:59:32 -0600

    pagina de visualizacion de insignias iniciada

--- src/app/pages/badges/badges.component.ts
+++ src/app/pages/badges/badges.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { IBadge } from "../../interfaces";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-badges',
+  selector: "app-badges",
   standalone: true,
-  imports: [],
-  templateUrl: './badges.component.html',
-  styleUrl: './badges.component.scss'
+  imports: [CommonModule],
+  templateUrl: "./badges.component.html",
+  styleUrl: "./badges.component.scss",
 })
 export class BadgesComponent {
-
+  public badges: IBadge[] = [
+    {
+      badgeId: 1,
+      title: "Badge 1",
+      description: "Description 1",
+      image: "../../../assets/icons/LogoP.png",
+    },
+    {
+      badgeId: 2,
+      title: "Badge 2",
+      description: "Description 2",
+      image: "../../../assets/icons/LogoP.png",
+    },
+    {
+      badgeId: 3,
+      title: "Badge 3",
+      description: "Description 3",
+      image: "../../../assets/icons/LogoP.png",
+    },
+  ];
 }
