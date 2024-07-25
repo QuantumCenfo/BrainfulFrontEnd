02dac0664e0f9369b4069d3c8b46c96ab6245761
Daniel Gomez <dgnavarro06@gmail.com>
2024-07-25 08:51:14 -0600

    Confimarciones para actualizar y borrar elementos

--- src/app/services/badge.service.ts
+++ src/app/services/badge.service.ts
@@ -85,55 +85,74 @@ export class BadgeService extends BaseService<IBadge> {
   }
 
   handleUpdateBadge(badge: IBadge, imageFile: File) {
-    if (imageFile) {
-      this.updateBadge(badge, imageFile).subscribe({
-        next: (res: any) => {
-          const updatedBadge = this.badgeSignal().map((b: IBadge) =>
-            b.badgeId === badge.badgeId ? badge : b
-          );
-          this.badgeSignal.set(updatedBadge);
-          console.log("Response: ", res);
-          console.log("Badge updated successfully");
-          Swal.fire({
-            title: "¡Éxito!",
-            text: "La insignia ha sido actualizada",
-            icon: "success",
-            iconColor: "white",
-            color: "white",
-            background: "#16c2d5",
-            confirmButtonColor: "#ff9f1c",
-          });
-        },
-        error: (err: any) => {
-          console.log("Error: ", err);
-        },
-      });
-    } else {
-      Swal.fire({
-        title: "Oops...",
-        text: "Porfavor subir una imagen",
-        icon: "warning",
-        iconColor: "white",
-        color: "white",
-        background: "#16c2d5",
-        confirmButtonColor: "#ff9f1c",
-      });
-    }
+    Swal.fire({
+      title: "Esta seguro que desea actualizar la medalla?",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, actualizar",
+    }).then((res) => {
+      if (imageFile) {
+        this.updateBadge(badge, imageFile).subscribe({
+          next: (res: any) => {
+            const updatedBadge = this.badgeSignal().map((b: IBadge) =>
+              b.badgeId === badge.badgeId ? badge : b
+            );
+            this.badgeSignal.set(updatedBadge);
+            console.log("Response: ", res);
+            console.log("Badge updated successfully");
+            Swal.fire({
+              title: "¡Éxito!",
+              text: "La insignia ha sido actualizada",
+              icon: "success",
+              iconColor: "white",
+              color: "white",
+              background: "#16c2d5",
+              timer: 2000,
+            });
+          },
+          error: (err: any) => {
+            console.log("Error: ", err);
+          },
+        });
+      } else {
+        Swal.fire({
+          title: "Oops...",
+          text: "Porfavor subir una imagen",
+          icon: "warning",
+          iconColor: "white",
+          color: "white",
+          background: "#16c2d5",
+          confirmButtonColor: "#ff9f1c",
+        });
+      }
+    });
   }
 
   deleteBadge(badgeId: number) {
-    this.del(badgeId).subscribe({
-      next: () => {
-        const deletedBadge = this.badgeSignal().filter(
-          (badge: IBadge) => badge.badgeId !== badgeId
-        );
-        this.badgeSignal.set(deletedBadge);
+    Swal.fire({
+      title: "Seguro que desea eliminar la medalla?",
+      text: "No podrá recuperar la información",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, eliminar",
+    }).then((res) => {
+      this.del(badgeId).subscribe({
+        next: () => {
+          const deletedBadge = this.badgeSignal().filter(
+            (badge: IBadge) => badge.badgeId !== badgeId
+          );
+          this.badgeSignal.set(deletedBadge);
 
-        console.log("Badge deleted successfully");
-      },
-      error: (err: any) => {
-        console.error("Error deleting badge", err);
-      },
+          console.log("Badge deleted successfully");
+        },
+        error: (err: any) => {
+          console.error("Error deleting badge", err);
+        },
+      });
     });
   }
 }
