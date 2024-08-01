aa048eb2911c2529f01e02012cadf299888ec0dc
JOSE IGNACIO VARGAS JARAMILLO <135558719+Getz7@users.noreply.github.com>
2024-08-01 09:05:35 -0600

    pruebas pt 2

---
+++ src/app/components/participation-outdoor-list/participation-outdoor-list.component.spec.ts
@@ -0,0 +1,70 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ParticipationOutdoorListComponent } from './participation-outdoor-list.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ParticipationOutdoorService } from '../../services/participation-outdoor.service';
+import { ModalComponent } from '../modal/modal.component';
+import { ValidationModalComponent } from '../validation-modal/validation-modal.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { IPartcipationOutdoor } from '../../interfaces';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+describe('ParticipationOutdoorListComponent', () => {
+  let component: ParticipationOutdoorListComponent;
+  let fixture: ComponentFixture<ParticipationOutdoorListComponent>;
+  let mockModalService: jasmine.SpyObj<NgbModal>;
+  let mockParticipationService: jasmine.SpyObj<ParticipationOutdoorService>;
+
+  beforeEach(async () => {
+    mockModalService = jasmine.createSpyObj('NgbModal', ['open']);
+    mockParticipationService = jasmine.createSpyObj('ParticipationOutdoorService', ['getParticipations']);
+
+    await TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        FormsModule,
+        ModalComponent,
+        ValidationModalComponent,
+        HttpClientTestingModule, 
+        ParticipationOutdoorListComponent // Import standalone component here
+      ],
+      providers: [
+        { provide: NgbModal, useValue: mockModalService },
+        { provide: ParticipationOutdoorService, useValue: mockParticipationService }
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ParticipationOutdoorListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should display participation list correctly', () => {
+    const mockParticipationList: IPartcipationOutdoor[] = [
+      { participationOutdoorId: 1, challengeOutdoor: { name: 'Challenge 1', description: 'Description 1' }, fechaPublicacion: "2024-06-08", status: 'pendiente', user: { name: 'User 1' },evidence:"prueba 1" },
+      { participationOutdoorId: 2, challengeOutdoor: { name: 'Challenge 2', description: 'Description 2' }, fechaPublicacion:"2024-02-21", status: 'pendiente', user: { name: 'User 2' },evidence:"prueba 1" }
+    ];
+    component.participationList = mockParticipationList;
+    fixture.detectChanges();
+
+    const rows = fixture.nativeElement.querySelectorAll('tbody tr');
+    expect(rows.length).toBe(mockParticipationList.length);
+    expect(rows[0].textContent).toContain('Challenge 1');
+    expect(rows[1].textContent).toContain('Challenge 2');
+  });
+
+  it('should call showDetail and open the modal', () => {
+    const mockParticipation: IPartcipationOutdoor = { participationOutdoorId: 1, challengeOutdoor: { name: 'Challenge 1', description: 'Description 1' }, fechaPublicacion: "2024-06-08", status: 'pendiente', user: { name: 'User 1' },evidence:"prueba 1" };
+    const mockModal = { show: jasmine.createSpy('show') };
+    spyOn(component.modalService, 'open').and.returnValue(mockModal as any);
+    component.showDetail(mockParticipation, mockModal);
+
+    expect(component.currentParticipation).toEqual(mockParticipation);
+    expect(mockModal.show).toHaveBeenCalled();
+  });
+
+
+});
---
+++ src/app/components/try-again-modal/try-again-modal.component.spec.ts
@@ -0,0 +1,38 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { TryAgainModalComponent } from './try-again-modal.component';
+
+describe('TryAgainModalComponent', () => {
+  let component: TryAgainModalComponent;
+  let fixture: ComponentFixture<TryAgainModalComponent>;
+  let activeModal: NgbActiveModal;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TryAgainModalComponent], // Import the standalone component directly
+      providers: [NgbActiveModal]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TryAgainModalComponent);
+    component = fixture.componentInstance;
+    activeModal = TestBed.inject(NgbActiveModal);
+    fixture.detectChanges();
+  });
+
+ 
+
+  it('should close the modal with "tryAgain" when tryAgain is called', () => {
+    spyOn(activeModal, 'close');
+    component.tryAgain();
+    expect(activeModal.close).toHaveBeenCalledWith('tryAgain');
+  });
+
+  it('should close the modal with "goToAnotherView" when goToAnotherView is called', () => {
+    spyOn(activeModal, 'close');
+    component.goToAnotherView();
+    expect(activeModal.close).toHaveBeenCalledWith('goToAnotherView');
+  });
+});
