2608ec04cff05f8d596429b8c190cf5742c47546
Daniel Gomez <dgnavarro06@gmail.com>
2024-08-01 23:14:27 -0600

    refactro de tabla partcipation, badge list vista admin casi lista

--- src/app/components/badge-list/badge-list.component.ts
+++ src/app/components/badge-list/badge-list.component.ts
@@ -18,15 +18,17 @@ import { UserBadgeService } from "../../services/user-badge.service";
 })
 export class BadgeListComponent {
   @Input() badgeList: IBadge[] = [];
-
-  private badgeService = inject(BadgeService);
-
-  public modalService = inject(NgbModal);
-
   @Input() isAllowed: boolean = false;
+  flippedIndexes: number[] = []; // Array to track flipped cards
+
+  colors = ["#9816D5", "#2f9ca8", "#65b32a", "#FF9F1C"];
 
   public selectedBadge: IBadge = {};
   public selectedFile: File | null = null;
+  isFlipped: boolean = false;
+
+  private badgeService = inject(BadgeService);
+  public modalService = inject(NgbModal);
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes["isAllowed"]) {
@@ -47,5 +49,56 @@ export class BadgeListComponent {
 
   delteBadge(badgeID: number) {
     this.badgeService.deleteBadge(badgeID);
+  }
+
+  getGradient(index: number): string {
+    const color = this.colors[index % this.colors.length];
+    return this.generateGradient(color);
+  }
+  getBoxShadow(index: number): string {
+    const color = this.colors[index % this.colors.length];
+    return this.generateBoxShadow(color);
+  }
+  getColor(index: number): string {
+    return this.colors[index % this.colors.length];
+  }
+
+  generateBoxShadow(color: string): string {
+    const darkerColor = this.lightenColor(color, -20); // You can adjust the lightness
+    return `2px 0px 0px 3px ${darkerColor}`;
+  }
+
+  generateGradient(color: string): string {
+    const lightColor = this.lightenColor(color, 30); // You can adjust the lightness
+    return `linear-gradient(45deg, ${color} 0%, ${lightColor} 57%, ${color} 100%)`;
+  }
+
+  lightenColor(color: string, percent: number): string {
+    const num = parseInt(color.slice(1), 16),
+      amt = Math.round(2.55 * percent),
+      R = (num >> 16) + amt,
+      G = ((num >> 8) & 0x00ff) + amt,
+      B = (num & 0x0000ff) + amt;
+    return `#${(
+      0x1000000 +
+      (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
+      (G < 255 ? (G < 1 ? 0 : G) : 255) * 0x100 +
+      (B < 255 ? (B < 1 ? 0 : B) : 255)
+    )
+      .toString(16)
+      .slice(1)
+      .toUpperCase()}`;
+  }
+
+  flipCard(index: number) {
+    const flippedIndex = this.flippedIndexes.indexOf(index);
+    if (flippedIndex >= 0) {
+      // Card is already flipped, so unflip it
+      this.flippedIndexes.splice(flippedIndex, 1);
+    } else {
+      // Flip the card
+      this.flippedIndexes.push(index);
+    }
+    console.log("Flipped Indexes: ", this.flippedIndexes);
   }
 }
--- src/app/components/participation-outdoor-list/participation-outdoor-list.component.ts
+++ src/app/components/participation-outdoor-list/participation-outdoor-list.component.ts
@@ -1,27 +1,40 @@
-
-import { Component, inject, Input } from '@angular/core';
-import { IPartcipationOutdoor } from '../../interfaces';
-import { ParticipationOutdoorService } from '../../services/participation-outdoor.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { CommonModule } from '@angular/common';
-import { ModalComponent } from '../modal/modal.component';
-import { ValidationModalComponent } from '../validation-modal/validation-modal.component';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, Input } from "@angular/core";
+import { IPartcipationOutdoor } from "../../interfaces";
+import { ParticipationOutdoorService } from "../../services/participation-outdoor.service";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { CommonModule } from "@angular/common";
+import { ModalComponent } from "../modal/modal.component";
+import { ValidationModalComponent } from "../validation-modal/validation-modal.component";
+import { FormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatPaginatorModule, PageEvent } from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableDataSource, MatTableModule } from "@angular/material/table";
 
 @Component({
-  selector: 'app-participation-outdoor-list',
+  selector: "app-participation-outdoor-list",
   standalone: true,
-  imports: [CommonModule,ModalComponent,ValidationModalComponent,CommonModule,
-    FormsModule,],
-  templateUrl: './participation-outdoor-list.component.html',
-  styleUrl: './participation-outdoor-list.component.scss'
+  imports: [
+    CommonModule,
+    ModalComponent,
+    ValidationModalComponent,
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    FormsModule,
+  ],
+  templateUrl: "./participation-outdoor-list.component.html",
+  styleUrl: "./participation-outdoor-list.component.scss",
 })
 export class ParticipationOutdoorListComponent {
   @Input() participationList: IPartcipationOutdoor[] = [];
   public selectedItem: IPartcipationOutdoor = {};
 
-  public currentParticipation: IPartcipationOutdoor = {
-  };
+  dataSource = new MatTableDataSource<IPartcipationOutdoor>([]);
+
+  public currentParticipation: IPartcipationOutdoor = {};
   private participationService = inject(ParticipationOutdoorService);
 
   public modalService = inject(NgbModal);
@@ -30,5 +43,12 @@ export class ParticipationOutdoorListComponent {
     modal.show();
   }
 
-
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.dataSource.data = this.participationList;
+  }
+  pageEvent(event: PageEvent) {
+    console.log("Event", event);
+  }
 }
