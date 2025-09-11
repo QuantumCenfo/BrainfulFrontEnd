975a66822a505933dcbc61cf95c39b49728ccf0f
JOSE IGNACIO VARGAS JARAMILLO <135558719+Getz7@users.noreply.github.com>
2025-09-11 08:21:20 -0600

    changes no funcionales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { ChatService } from "./services/chat.service";
+
 
 @Component({
   selector: "app-root",
@@ -10,6 +10,6 @@ import { ChatService } from "./services/chat.service";
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  public chatService = inject(ChatService);
+
   title = "brainful";
 }
